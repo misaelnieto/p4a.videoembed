6fb558b60bcdc05767be1510a049816da9e51c96
rocky <rocky@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2007-08-11 19:40:52 +0000

    Enabled flv player configuration to happen via IFlvPlayerConfig utility. Default behaviour is still invalid.  This component is meant to be provided by p4a.videoembed consumers.

--- p4a/videoembed/providers/genericflv.py
+++ p4a/videoembed/providers/genericflv.py
@@ -3,8 +3,9 @@ import re
 from urlparse import urlunsplit
 import urllib2
 from urllib import quote, quote_plus
-from zope.interface import implements, implementer
-from zope.component import adapts, adapter
+from zope.interface import implements, implementer, Interface
+from zope.schema import TextLine
+from zope.component import adapts, adapter, queryUtility
 from p4a.videoembed.utils import break_url
 from p4a.videoembed.interfaces import provider
 from p4a.videoembed.interfaces import IEmbedCode
@@ -23,29 +24,40 @@ def flv_check(url):
 
 flv_check.index = 10100
 
-# FLV player url (requires the flv player from http://www.jeroenwijering.com/)
-# indicate the url to your copy of that player here
+class IFlvPlayerConfig(Interface):
+    """Configuration for accessing the RESTful api for youtube."""
+
+    player_url = TextLine(title=u'URL to Player',
+                          description=u'FLV player url (requires the flv '
+                                      u'player from '
+                                      u'http://www.jeroenwijering.com/)',
+                          required=True)
 
 FLV_PLAYER_URL = "http://location/path/to/flvplayer.swf"
 
 @adapter(str, int)
 @implementer(IEmbedCode)
 def flv_generator(url, width):
-    """ A quick check for the right url, this one requires a direct
+    ''' A quick check for the right url, this one requires a direct
     flv link:
 
     >>> print flv_generator('http://blip.tv/file/get/SomeVideo.flv', width=400)
     <embed src="http://location/path/to/flvplayer.swf" width="400" height="320" bgcolor="#FFFFFF" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" flashvars="file=http://blip.tv/file/get/SomeVideo.flv&autostart=true"></embed>
 
-    """
+    '''
     tag = []
     height = int(round(0.8*width))
+
+    player_url = FLV_PLAYER_URL
+    config = queryUtility(IFlvPlayerConfig)
+    if config is not None:
+        player_url = config.player_url or player_url
 
     video_url = url
     tag.append('<embed src="%s" width="%s" height="%s" bgcolor="#FFFFFF" '
         'type="application/x-shockwave-flash" '
         'pluginspage="http://www.macromedia.com/go/getflashplayer" '
-        'flashvars="file=%s&autostart=true">'%(FLV_PLAYER_URL, width, height,
-                                                video_url))
+        'flashvars="file=%s&autostart=true">'%(player_url, width, height,
+                                               video_url))
     tag.append('</embed>')
     return u''.join(tag)
