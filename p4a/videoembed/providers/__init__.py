a674a50ae016256b65d684365518279bc795bdc9
rosspatterson <rosspatterson@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-04-02 19:49:16 +0000

    Fix tests after Nate's brightcove additions

--- p4a/videoembed/providers/__init__.py
+++ p4a/videoembed/providers/__init__.py
@@ -585,7 +585,7 @@ def brightcove_generator(url, width):
     """ A quick check for the right url
 
     >>> print brightcove_generator('http://www.brightcove.tv/title.jsp?title=1320139248', width=400)
-    <embed src='http://www.brightcove.tv/playerswf' bgcolor='#FFFFFF' flashVars='initVideoId=1320139248&servicesURL=http://www.brightcove.tv&viewerSecureGatewayURL=https://www.brightcove.tv&cdnURL=http://admin.brightcove.com&autoStart=false' base='http://admin.brightcove.com' name='bcPlayer' width='400' height='339' allowFullScreen='true' allowScriptAccess='always' seamlesstabbing='false' type='application/x-shockwave-flash' swLiveConnect='true' pluginspage='http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash'></embed>
+    <embed src="http://www.brightcove.tv/playerswf" bgcolor="#FFFFFF" flashVars="initVideoId=1320139248&servicesURL=http://www.brightcove.tv&viewerSecureGatewayURL=https://www.brightcove.tv&cdnURL=http://admin.brightcove.com&autoStart=false" base="http://admin.brightcove.com" name="bcPlayer" width="400" height="339" allowFullScreen="true" allowScriptAccess="always" seamlesstabbing="false" type="application/x-shockwave-flash" swLiveConnect="true" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash"></embed>
 
     """
     tag = []
